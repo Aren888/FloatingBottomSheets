5b2994bb3e488164c5e28f4c7ebed46b5581e5e4
v-AMusayelyan <v-amusayelyan@quantumftg.com>
2024-08-06 15:14:25 +0400

    Initial Commit

---
+++ FloatingBottomSheets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FloatingBottomSheets
+//
+//  Created by Solicy Ios on 06.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FloatingBottomSheets/FloatingBottomSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingBottomSheetsApp.swift
+//  FloatingBottomSheets
+//
+//  Created by Solicy Ios on 06.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingBottomSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
